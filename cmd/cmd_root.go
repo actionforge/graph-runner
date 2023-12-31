bddcc36a0b90d29fec2d3ac7642a77d68aed3f79
Sebastian Rath <git@sebastianrath.io>
2023-12-31 13:30:19 -0500

    Bake build version into binary (#4)

--- cmd/cmd_root.go
+++ cmd/cmd_root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"actionforge/graph-runner/core"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -9,7 +10,7 @@ import (
 var cmdRoot = &cobra.Command{
 	Use:     "graph-runner",
 	Short:   "Graph runner is a tool for running action graphs.",
-	Version: "0.1a",
+	Version: core.GetFulllVersionInfo(),
 	Run: func(cmd *cobra.Command, args []string) {
 		_ = cmd.Help()
 	},
