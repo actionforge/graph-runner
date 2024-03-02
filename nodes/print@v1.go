0ed79f6193c1cc3a2c390b7b95e5a49d49143956
Sebastian Rath <git@sebastianrath.io>
2024-03-02 13:24:46 -0500

    Add print node

---
+++ nodes/print@v1.go
@@ -0,0 +1,44 @@
+package nodes
+
+import (
+	"actionforge/graph-runner/core"
+	ni "actionforge/graph-runner/node_interfaces"
+	u "actionforge/graph-runner/utils"
+	_ "embed"
+	"fmt"
+)
+
+//go:embed print@v1.yml
+var printDefinition string
+
+type PrintNode struct {
+	core.NodeBaseComponent
+	core.Executions
+	core.Inputs
+}
+
+func (n *PrintNode) ExecuteImpl(c core.ExecutionContext) error {
+
+	value, err := core.InputValueById[any](c, n.Inputs, ni.Print_v1_Input_value)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%v\n", value)
+
+	err = n.Execute(n.Executions[core.OutputId(ni.Print_v1_Output_exec)], c)
+	if err != nil {
+		return u.Throw(err)
+	}
+
+	return nil
+}
+
+func init() {
+	err := core.RegisterNodeFactory(printDefinition, func(ctx interface{}, nodeDef map[any]any) (core.NodeRef, error) {
+		return &PrintNode{}, nil
+	})
+	if err != nil {
+		panic(err)
+	}
+}
