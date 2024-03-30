bb8eb531a11721d34fa05fb7819c6b32474f76eb
Sebastian Rath <git@sebastianrath.io>
2024-03-30 00:08:21 -0400

    Fix warning  about tautological

--- nodes/negate@v1.go
+++ nodes/negate@v1.go
@@ -15,19 +15,48 @@ type NegateNode struct {
 	core.Outputs
 }
 
+func isZeroValue(input any) bool {
+	switch v := input.(type) {
+	case bool:
+		return !v
+	case string:
+		return v == ""
+	case int:
+		return v == 0
+	case int8:
+		return v == 0
+	case int16:
+		return v == 0
+	case int32:
+		return v == 0
+	case int64:
+		return v == 0
+	case uint:
+		return v == 0
+	case uint8:
+		return v == 0
+	case uint16:
+		return v == 0
+	case uint32:
+		return v == 0
+	case uint64:
+		return v == 0
+	case float32:
+		return v == float32(0.0)
+	case float64:
+		return v == float64(0.0)
+	default:
+		return false
+	}
+}
+
 func (n *NegateNode) OutputValueById(c core.ExecutionContext, outputId core.OutputId) (interface{}, error) {
 	input, errA := core.InputValueById[any](c, n.Inputs, ni.Bool_or_v1_Input_input)
 	if errA != nil {
 		return nil, errA
 	}
 
-	ai := bool(input == true)
-	bi := bool(input != "")
-	ci := bool(input != nil)
-	di := bool(input != 0)
-	ei := bool(input != 0.0)
-
-	return !(ai || bi || ci || di || ei), nil
+	return !isZeroValue(input), nil
 }
 
 func init() {
